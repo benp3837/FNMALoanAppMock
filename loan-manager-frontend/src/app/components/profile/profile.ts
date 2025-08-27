768ef2a958f0befa91c2827a6263457ab41b5149
Benp3837 <petruzziellbj@alumni.vcu.edu>
2025-08-27 15:17:39 -0400

    vibe coded user profile

--- loan-manager-frontend/src/app/components/profile/profile.ts
+++ loan-manager-frontend/src/app/components/profile/profile.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-profile',
@@ -7,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './profile.css'
 })
 export class Profile {
+
+  constructor(public authService:AuthService){}
+
+
 
 }
